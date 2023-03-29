7a9f0af1a22dd1ee1760f172d3f6a1b11f302d6d
SunAltahri <sunshine.arts.3@gmail.com>
2023-03-29 05:55:36 +0300

    e

--- src/app.js
+++ src/app.js
@@ -1,198 +1,143 @@
-// 
-const form = document.querySelector("#searchForm");
-const input = form.elements.query;
-let timeoutId;
+const searchForm = document.getElementById('search-form');
+const searchInput = document.querySelector('input[type="search"]');
+const suggestionsContainer = document.getElementById('suggestions-container');
+const suggestionsList = document.getElementById('suggestions-list');
+const recommendationsContainer = document.getElementById('recommendations-container');
 
-const getShows = async (query) => {
-  const response = await axios.get(`https://api.tvmaze.com/search/shows?q=${query}`);
-  return response.data.map(result => result.show);
+// دالة للاستعلام عن معلومات العروض باستخدام API
+async function getShows(query) {
+  const response = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
+  const data = await response.json();
+  return data.map(result => result.show);
 }
 
-window.makeImages = (shows) => {
-  for(let result of shows) {
-    if(result.image && result.name && result.rating.average && result.genres.length && result.premiered) {
-      const div = document.createElement("div");
-      div.classList.add("movie-container");
-
-      const img = document.createElement("img");
-      img.src = result.image.medium;
-      div.append(img);
-
-      const name = document.createElement("p");
-      name.innerHTML = "<strong>Name: </strong>" + result.name;
-      div.append(name);
-
-      const rating = document.createElement("p");
-      rating.innerHTML = "<strong>Rating: </strong>" + result.rating.average;
-      div.append(rating);
-
-      const genres = document.createElement("p");
-      genres.innerHTML = "<strong>Genres: </strong>" + result.genres.join(", ");
-      div.append(genres);
-
-      const premiered = document.createElement("p");
-      premiered.innerHTML = "<strong>Premiered: </strong>" + result.premiered;
-      div.append(premiered);
-
-      document.body.append(div);
-    }
-  }
-}
-
-const getSuggestions = async (query) => {
-  const shows = await getShows(query);
-  return shows.map(show => ({
-    name: show.name,
-    image: show.image && show.image.medium,
-    rating: show.rating && show.rating.average,
-    genres: show.genres && show.genres.join(", "),
-    premiered: show.premiered
-  }));
-}
-
-// عرض قائمة الاقتراحات وعنوانها
-const suggestionsContainer = document.createElement("div");
-const suggestionsTitle = document.createElement("h2");
-suggestionsTitle.textContent = "Suggestions:";
-suggestionsContainer.append(suggestionsTitle);
-
-// إنشاء قائمة لعرض الاقتراحات
-const suggestionsList = document.createElement("ul");
-
-const handleInput = async () => {
-  const searchTerm = input.value;
-  if (searchTerm.trim() === "") {
-    // مسح القائمة عندما يكون حقل البحث خالياً
-    suggestionsContainer.style.display = "none";
-    while (suggestionsList.firstChild) {
-      suggestionsList.removeChild(suggestionsList.firstChild);
-    }
-    return;
-  }
-
-  // الحصول على الاقتراحات وعرضها في القائمة
-  const suggestions = await getSuggestions(searchTerm);
+// دالة لإظهار الاقتراحات في suggestionsList
+function showSuggestions(shows) {
+  // إزالة جميع العناصر السابقة من suggestionsList
   while (suggestionsList.firstChild) {
     suggestionsList.removeChild(suggestionsList.firstChild);
   }
-  for (let suggestion of suggestions) {
-    const li = document.createElement("li");
 
-    // إضافة الصورة إذا كانت متوفرة
-    if (suggestion.image) {
-      const img = document.createElement("img");
-      img.src = suggestion.image;
-      img.alt = suggestion.name;
-      li.append(img);
-    }
+  // إنشاء وإضافة العناصر الجديدة إلى suggestionsList
+  shows.forEach(show => {
+    const suggestionItem = document.createElement('li');
+    suggestionItem.classList.add('suggestion-item');
+    suggestionItem.innerHTML = `
+      <div class="suggestion-details">
+        <h3>${show.name}</h3>
+      </div>
+    `;
+    suggestionItem.addEventListener('click', handleSuggestionClick);
+    suggestionsList.appendChild(suggestionItem);
+  });
 
-    // إضافة اسم العرض والتقييم والنوع والتاريخ
-    const div = document.createElement("div");
-    div.classList.add("suggestion-details");
-    const name = document.createElement("p");
-    name.innerHTML = "<strong>Name: </strong>" + suggestion.name;
-    div.append(name);
-    if (suggestion.rating) {
-      const rating = document.createElement("p");
-      rating.innerHTML = "<strong>Rating: </strong>" + suggestion.rating;
-      div.append(rating);
-    }
-    if (suggestion.genres) {
-      const genres = document.createElement("p");
-      genres.innerHTML = "<strong>Genres: </strong>" + suggestion.genres;
-      div.append(genres);
-    }
-    if (suggestion.premiered) {
-      const premiered = document.createElement("p");
-      premiered.innerHTML = "<strong>Premiered: </strong>" + suggestion.premiered;
-      div.append(premiered);
-    }
-    li.append(div);
+  // إظهار suggestionsContainer إذا كانت الأفكار غير فارغة ، وإلا فإنه يجب أن يتم إخفاؤه
+  if (shows.length > 0) {
+    suggestionsContainer.style.display = 'block';
+  } else {
+    suggestionsContainer.style.display = 'none';
+  }
+}
 
-    suggestionsList.append(li);
+// دالة للتعامل مع عمليات البحث
+const handleSearch = async (event) => {
+  // منع تحميل الصفحة بعد النقر على زر إرسال
+  event.preventDefault();
+
+  // استخراج قيمة مدخل البحث وتنظيفها
+  const searchValue = searchInput.value.trim().toLowerCase();
+  if (!searchValue) {
+    return;
   }
 
-  // إضافة القائمة إلى الصفحة
-  if (suggestions.length > 0) {
-    suggestionsContainer.style.display = "block";
-    suggestionsContainer.append(suggestionsList);
-    document.body.append(suggestionsContainer);
+  // إخفاء suggestionsContainer
+  suggestionsContainer.style.display = 'none';
 
-} else {
-suggestionsContainer.style.display = "none";
-}
+  // استعلام عن معلومات العروض باستخدام القيمة المدخلة
+  const shows = await getShows(searchValue);
+
+  // عرض التوصيات في recommendationsContainer
+  makeImages(shows);
 };
 
-const handleSubmit = async (event) => {
-event.preventDefault();
-const searchTerm = input.value;
-const shows = await getShows(searchTerm);
+// إضافة حدث input لمدخل البحث
+searchInput.addEventListener('input', async () => {
+  // استخراج قيمة مدخل البحث وتنظيفها
+  const searchValue = searchInput.value.trim().toLowerCase();
+  if (!searchValue) {
+    return;
+  }
 
-if (shows.length > 0) {
-// إذا وجد العرض المطلوب بالضبط، يتم عرضه أولاً
-const show = shows.find(show => show.name.toLowerCase() === searchTerm.toLowerCase());
-if (show) {
-window.makeImages([show]);
-return;
-}
-}
+  // استعلام عن معلومات العروض باستخدام القيمة المدخلة
+  const shows = await getShows(searchValue);
 
-// عرض قائمة الاقتراحات
-const suggestions = await getSuggestions(searchTerm);
-
-while (suggestionsList.firstChild) {
-suggestionsList.removeChild(suggestionsList.firstChild);
-}
-
-for (let suggestion of suggestions) {
-const li = document.createElement("li");
-
-// إضافة الصورة إذا كانت متوفرة
-if (suggestion.image) {
-  const img = document.createElement("img");
-  img.src = suggestion.image;
-  img.alt = suggestion.name;
-  li.append(img);
-}
-
-// إضافة اسم العرض والتقييم والنوع والتاريخ
-const div = document.createElement("div");
-div.classList.add("suggestion-details");
-const name = document.createElement("p");
-name.innerHTML = "<strong>Name: </strong>" + suggestion.name;
-div.append(name);
-if (suggestion.rating) {
-  const rating = document.createElement("p");
-  rating.innerHTML = "<strong>Rating: </strong>" + suggestion.rating;
-  div.append(rating);
-}
-if (suggestion.genres) {
-  const genres = document.createElement("p");
-  genres.innerHTML = "<strong>Genres: </strong>" + suggestion.genres;
-  div.append(genres);
-}
-if (suggestion.premiered) {
-  const premiered = document.createElement("p");
-  premiered.innerHTML = "<strong>Premiered: </strong>" + suggestion.premiered;
-  div.append(premiered);
-}
-li.append(div);
-
-suggestionsList.append(li);
-}
-
-// إضافة القائمة إلى الصفحة
-if (suggestions.length > 0) {
-suggestionsContainer.style.display = "block";
-suggestionsContainer.append(suggestionsList);
-document.body.append(suggestionsContainer);
-} else {
-suggestionsContainer.style.display = "none";
-}
-};
-
-form.addEventListener("submit", handleSubmit);
-input.addEventListener("input", () => {
-clearTimeout(timeoutId);
-timeoutId = setTimeout(handleInput, 500);
+  // عرض النتائج في suggestionsList
+  showSuggestions(shows.slice(0, 5));
 });
+
+// إنشاء دالة جديدة لمعالجة حدث النقر على الاقتراحات
+const handleSuggestionClick = async (event) => {
+  // إخفاء suggestionsContainer
+  suggestionsContainer.style.display = 'none';
+
+  // استخراج اسم العرض المحدد
+  const name = event.currentTarget.querySelector('.suggestion-details h3').textContent;
+
+  // استعلام عن معلومات العروض باستخدام الاسم
+  const shows = await getShows(name);
+
+  // عرض التوصيات في recommendationsContainer
+  makeImages(shows);
+};
+
+// دالة لإنشاء عناصر HTML img وعرضها في recommendationsContainer
+function makeImages(shows) {
+  // إزالة جميع العناصر السابقة من recommendationsContainer
+  while (recommendationsContainer.firstChild) {
+    recommendationsContainer.removeChild(recommendationsContainer.firstChild);
+  }
+
+  // عرض الصور لكل عرض في recommendationsContainer
+  shows.forEach(show => {
+    const container = document.createElement('div');
+    container.classList.add('show-container');
+
+    const image = document.createElement('img');
+    image.classList.add('recommendation-image');
+    image.src = show.image ? show.image.medium : '';
+    container.appendChild(image);
+
+    const details = document.createElement('div');
+    details.classList.add('show-details');
+
+    const name = document.createElement('h3');
+    name.textContent = show.name;
+    details.appendChild(name);
+
+    if (show.premiered) {
+      const premiered = document.createElement('p');
+      premiered.textContent = `Premiered: ${show.premiered}`;
+      details.appendChild(premiered);
+    }
+
+    if (show.rating && show.rating.average) {
+      const rating = document.createElement('p');
+      rating.textContent = `Rating: ${show.rating.average}/10`;
+      details.appendChild(rating);
+    }
+
+    if (show.genres && show.genres.length > 0) {
+      const genres = document.createElement('p');
+      genres.textContent = `Genres: ${show.genres.join(', ')}`;
+      details.appendChild(genres);
+    }
+
+    container.appendChild(details);
+
+    recommendationsContainer.appendChild(container);
+  });
+}
+
+
+// إضافة حدث submit للنموذج للبحث عن عروض تلفزيونية
+searchForm.addEventListener('submit', handleSearch);
