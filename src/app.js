3acb2746b037256003b9a84fb8175dbe7958308b
SunAltahri <sunshine.arts.3@gmail.com>
2023-03-30 04:48:26 +0300

    edit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+//.ENV
+import dotenv from 'dotenv'
+dotenv.config()
+
+//
 const searchForm = document.getElementById('search-form');
 const searchInput = document.querySelector('input[type="search"]');
 const suggestionsContainer = document.getElementById('suggestions-container');
@@ -6,9 +11,11 @@ const recommendationsContainer = document.getElementById('recommendations-contai
 
 // دالة للاستعلام عن معلومات العروض باستخدام API
 async function getShows(query) {
-  const response = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
+  const apiKey = process.env.APIKEY;
+  const response = await fetch(`https://imdb-api.com/en/API/SearchMovie/${apiKey}/${query}`);
   const data = await response.json();
-  return data.map(result => result.show);
+  console.log(data)
+  return data.results;
 }
 
 // دالة لإظهار الاقتراحات في suggestionsList
@@ -24,7 +31,8 @@ function showSuggestions(shows) {
     suggestionItem.classList.add('suggestion-item');
     suggestionItem.innerHTML = `
       <div class="suggestion-details">
-        <h3>${show.name}</h3>
+        <img class="img-details" src="${show.image}" alt="${show.title}">
+        <h3 class="title-details">${show.title}</h3>
       </div>
     `;
     suggestionItem.addEventListener('click', handleSuggestionClick);
@@ -81,14 +89,24 @@ const handleSuggestionClick = async (event) => {
   suggestionsContainer.style.display = 'none';
 
   // استخراج اسم العرض المحدد
-  const name = event.currentTarget.querySelector('.suggestion-details h3').textContent;
+  const title = event.currentTarget.querySelector('.suggestion-details h3').textContent;
 
-  // استعلام عن معلومات العروض باستخدام الاسم
-  const shows = await getShows(name);
+// استعلام عن معلومات العروض باستخدام الاسم
+  const shows = await getShows(title);
 
   // عرض التوصيات في recommendationsContainer
-  makeImages(shows);
+  makeImages([shows[0]]);
 };
+
+
+async function getMovieDetails(title) {
+   const apiKey = process.env.KEY; // تغيير YOUR_API_KEY إلى مفتاح API الخاص بك
+  const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`);
+  const data = await response.json();
+  console.log(data)
+  return data;
+}
+
 
 // دالة لإنشاء عناصر HTML img وعرضها في recommendationsContainer
 function makeImages(shows) {
@@ -98,38 +116,43 @@ function makeImages(shows) {
   }
 
   // عرض الصور لكل عرض في recommendationsContainer
-  shows.forEach(show => {
+ shows.forEach(async (show) => {
     const container = document.createElement('div');
     container.classList.add('show-container');
 
     const image = document.createElement('img');
     image.classList.add('recommendation-image');
-    image.src = show.image ? show.image.medium : '';
+    image.src = show.image ? show.image : '';
+    image.alt = show.title;
     container.appendChild(image);
 
     const details = document.createElement('div');
     details.classList.add('show-details');
 
-    const name = document.createElement('h3');
-    name.textContent = show.name;
-    details.appendChild(name);
+    const title = document.createElement('h3');
+    title.textContent = show.title;
+    details.appendChild(title);
 
-    if (show.premiered) {
-      const premiered = document.createElement('p');
-      premiered.textContent = `Premiered: ${show.premiered}`;
-      details.appendChild(premiered);
+    const movieDetails = await getMovieDetails(show.title);
+    if (movieDetails.Rated) {
+      const rated = document.createElement('p');
+      rated.textContent = `Rated: ${movieDetails.Rated}`;
+      details.appendChild(rated);
     }
-
-    if (show.rating && show.rating.average) {
+    if (movieDetails.imdbRating) {
       const rating = document.createElement('p');
-      rating.textContent = `Rating: ${show.rating.average}/10`;
+      rating.textContent = `IMDB Rating: ${movieDetails.imdbRating}/10`;
       details.appendChild(rating);
     }
-
-    if (show.genres && show.genres.length > 0) {
+    if (movieDetails.Genre) {
       const genres = document.createElement('p');
-      genres.textContent = `Genres: ${show.genres.join(', ')}`;
+      genres.textContent = `Genres: ${movieDetails.Genre}`;
       details.appendChild(genres);
+    }
+    if (movieDetails.Plot) {
+      const plot = document.createElement('p');
+      plot.textContent = `Plot: ${movieDetails.Plot}`;
+      details.appendChild(plot);
     }
 
     container.appendChild(details);
@@ -137,7 +160,6 @@ function makeImages(shows) {
     recommendationsContainer.appendChild(container);
   });
 }
-
 
 // إضافة حدث submit للنموذج للبحث عن عروض تلفزيونية
 searchForm.addEventListener('submit', handleSearch);
